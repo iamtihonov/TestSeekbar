f709f485d54e4997d7121dc9ad4172377a52945a
Artem Tihonov <iamtihonov@gmail.com>
2019-12-30 15:16:29 +0200

    Отображение двух

--- app/src/main/java/com/example/testseekbar/RangeSeekBar.kt
+++ app/src/main/java/com/example/testseekbar/RangeSeekBar.kt
@@ -19,7 +19,8 @@ import androidx.core.content.res.ResourcesCompat
  */
 class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
     private var screenHeight = 0f
-    private var thumbDrawable: Drawable? = null
+    private var leftThumbDrawable: Drawable? = null
+    private var rigthThumbDrawable: Drawable? = null
     private var activeLineColor: Int = 0
     private var defaultLineColor: Int = 0
     private var lineHeight: Int = 0
@@ -29,12 +30,15 @@ class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attr
     private var horizontalScrollPosition = 0
     private var leftThumbBound = Rect()
     private var leftThumbTouchBound = Rect()
+    private var rightThumbBound = Rect()
+    private var rightThumbTouchBound = Rect()
     private var touchPadding = 0
     private var seekBarPadding = 0
 
     init {
         context?.apply {
-            thumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.test, null)
+            leftThumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.test, null)
+            rigthThumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.test, null)
             activeLineColor = ContextCompat.getColor(this, R.color.blue_52_172_250)
             defaultLineColor = ContextCompat.getColor(this, R.color.grey_229_232_237)
             lineHeight = resources.getDimensionPixelSize(R.dimen.size_1dp)
@@ -42,58 +46,78 @@ class RangeSeekBar(context: Context?, attrs: AttributeSet?) : View(context, attr
             seekBarPadding = resources.getDimensionPixelSize(R.dimen.size_22dp)
         }
 
-        val width = thumbDrawable?.intrinsicWidth ?: 0
-        val height =  thumbDrawable?.intrinsicHeight ?: 0
+        val width = leftThumbDrawable?.intrinsicWidth ?: 0
+        val height = leftThumbDrawable?.intrinsicHeight ?: 0
         thumbDrawableSize = Size(width, height)
         leftThumbBound.left = seekBarPadding
         leftThumbBound.right = leftThumbBound.left + thumbDrawableSize.width
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
-        screenHeight = h.toFloat()
+    override fun onSizeChanged(newWidth: Int, newHeight: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(newWidth, newHeight, oldw, oldh)
+        screenHeight = newHeight.toFloat()
         leftThumbBound.top = ((screenHeight - thumbDrawableSize.height) / 2).toInt()
         leftThumbBound.bottom = (screenHeight - leftThumbBound.top).toInt()
         leftThumbTouchBound.top = leftThumbBound.top + touchPadding
         leftThumbTouchBound.bottom = leftThumbBound.bottom + touchPadding
+
+        rightThumbBound.right = newWidth - seekBarPadding
+        rightThumbBound.left = rightThumbBound.right - thumbDrawableSize.width
+        rightThumbBound.top = leftThumbBound.top
+        rightThumbBound.bottom = leftThumbBound.bottom
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
         val usingCanvas = canvas ?: return
 
-        thumbDrawable?.bounds = leftThumbBound
-        thumbDrawable?.draw(usingCanvas)
+        leftThumbDrawable?.bounds = leftThumbBound
+        leftThumbDrawable?.draw(usingCanvas)
+
+        rigthThumbDrawable?.bounds = rightThumbBound
+        rigthThumbDrawable?.draw(usingCanvas)
     }
 
     private val scrollListener = object : GestureDetector.SimpleOnGestureListener() {
-        private var downContains = false
+        private var clickLeftThumb = false
+        private var clickRightThumb = false
 
         override fun onDown(e: MotionEvent): Boolean {
-            downContains = leftThumbBound.contains(e.x.toInt(), e.y.toInt())
-            Log.d("testScroll", "onScroll() contains = $downContains")
-            return downContains
+            clickLeftThumb = leftThumbBound.contains(e.x.toInt(), e.y.toInt())
+            clickRightThumb = rightThumbBound.contains(e.x.toInt(), e.y.toInt())
+            Log.d("testScroll", "onScroll() contains = $clickLeftThumb")
+            return clickLeftThumb
         }
 
         @Suppress("CascadeIf")
-        override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float,
-                              distanceY: Float): Boolean {
-            if(!downContains) {
-                return false
-            }
-
-            Log.d("testScroll", "onScroll() x2 = ${e2?.x}")
-            e2?.let {
-                leftThumbBound.left = if(it.x <= thumbDrawableSize.width / 2 + seekBarPadding) {
+        override fun onScroll(
+            e1: MotionEvent?, e2: MotionEvent?, distanceX: Float,
+            distanceY: Float
+        ): Boolean {
+            val it = e2 ?: return true
+            if (clickLeftThumb) {
+                leftThumbBound.left = if (it.x <= thumbDrawableSize.width / 2 + seekBarPadding) {
                     seekBarPadding
-                } else if(it.x >= width - thumbDrawableSize.width / 2 - seekBarPadding) {
+                } else if (it.x >= width - thumbDrawableSize.width / 2 - seekBarPadding) {
                     width - thumbDrawableSize.width - seekBarPadding
                 } else {
                     (it.x - thumbDrawableSize.width / 2).toInt()
                 }
                 leftThumbBound.right = leftThumbBound.left + thumbDrawableSize.width
                 invalidate()
+            } else if (clickRightThumb) {
+                rightThumbBound.left = if (it.x <= thumbDrawableSize.width / 2 + seekBarPadding) {
+                    seekBarPadding
+                } else if (it.x >= width - thumbDrawableSize.width / 2 - seekBarPadding) {
+                    width - thumbDrawableSize.width - seekBarPadding
+                } else {
+                    (it.x - thumbDrawableSize.width / 2).toInt()
+                }
+                rightThumbBound.right = rightThumbBound.left + thumbDrawableSize.width
+                invalidate()
             }
+
+            Log.d("testScroll", "onScroll() x2 = ${e2.x}")
 
             return true
         }
