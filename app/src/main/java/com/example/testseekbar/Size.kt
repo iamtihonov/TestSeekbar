b91e9e26066a5d21bf0bcd9a192093f71dc0d83a
Artem <iamtihonov@gmail.com>
2021-03-27 15:05:43 +0200

    update_readmy

--- app/src/main/java/com/example/testseekbar/Size.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testseekbar
-
-class Size(var width: Int = 0, var height: Int = 0) {
-
-    override fun toString(): String {
-        return "width = $width, height = $height"
-    }
-}
